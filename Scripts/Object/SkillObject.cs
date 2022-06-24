a5d07a29bbed40d355821144fb2283071b5e04b5
GwiHyeon Kim <83810445+lnklie@users.noreply.github.com>
2022-06-24 15:01:38 +0900

    Update SkillObject.cs

--- Scripts/Object/SkillObject.cs
+++ Scripts/Object/SkillObject.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class SkillObject : MonoBehaviour
 {
-    private Rigidbody2D rig = null;
     private CircleCollider2D col = null;
 
     [SerializeField]
@@ -35,16 +34,14 @@ public class SkillObject : MonoBehaviour
         get { return maxCoolTime; }
         set { maxCoolTime = value; }
     }
-    [SerializeField]
-    private float coolTime = 0f;
-    public float CoolTime
+    private bool isSkillUse = false;
+    public bool IsSkillUse
     {
-        get { return coolTime; }
-        set { coolTime = value; }
+        get { return isSkillUse; }
+        set { isSkillUse = value; }
     }
     public void Awake()
     {
-        rig = this.GetComponent<Rigidbody2D>();
         col = this.GetComponent<CircleCollider2D>();
     }
     private void Start()
@@ -55,20 +52,76 @@ public class SkillObject : MonoBehaviour
     {
         if (target != null)
         {
-            coolTime += Time.deltaTime;
-            durationTime += Time.deltaTime;
-            this.transform.position = target.transform.position;
-            if (durationTime > maxDurationTime)
-            {
-                this.gameObject.SetActive(false);
-                durationTime = 0f;
-            }
-
-
+            if(isSkillUse)
+                StartCoroutine(CastingSkill());
+            RemoveSkill();
         }
         else
         {
-            Debug.Log("Ÿ���� �����ϴ�.");
+            Debug.Log("Å¸°ÙÀÌ ¾ø½À´Ï´Ù.");
         }
+    }
+    public IEnumerator CastingSkill()
+    {
+        isSkillUse = false;
+        this.transform.position = target.transform.position;
+        RaycastHit2D[] _hitRay = HitRay();
+        for(int i = 0; i < _hitRay.Length; i++)
+        {
+            if (_hitRay[i])
+            {
+                Status _status = _hitRay[i].collider.gameObject.GetComponent<Status>();
+                for (int j = 0; j < 3; j++)
+                {
+                    _status.CurHp -= damage / 3;
+                    _status.IsDamaged = true;
+                    Debug.Log(_status.CurHp);
+                    yield return new WaitForSeconds(durationTime / 3);
+                }
+                if (this.gameObject.CompareTag("Mercenary"))
+                {
+                    MercenaryAIController mercenary = this.gameObject.GetComponent<MercenaryAIController>();
+                    mercenary.IsAtk = true;
+                    if (mercenary.IsLastHit(_hitRay[i].collider.GetComponent<EnemyStatus>()))
+                    {
+                        mercenary.GetComponent<CharacterStatus>().CurExp += _hitRay[i].collider.GetComponent<EnemyStatus>().DefeatExp;
+                    }
+                }
+            }
+        }
+    }
+    //public IEnumerator ContinuousDamage(Status _status, int _damage)
+    //{
+    //    for(int i = 0; i < 3; i++ )
+    //    {
+    //        _status.CurHp -= _damage / 3;
+    //        yield return new WaitForSeconds(durationTime / 3);
+    //    }
+    //}
+    public void RemoveSkill()
+    {
+        durationTime += Time.deltaTime;
+        
+        if (durationTime > maxDurationTime)
+        {
+            this.gameObject.SetActive(false);
+            durationTime = 0f;
+        }
+
+    }
+    public int ReviseDamage(int _damage, int _depensivePower)
+    {
+        return Mathf.CeilToInt(_damage * (1 / (1 + _depensivePower)));
+    }
+    private RaycastHit2D[] HitRay()
+    {
+        // ·¹ÀÌ¸¦ ½î´Â ¿ªÇÒ
+        
+        RaycastHit2D[] ray = default;
+        if (this.gameObject.layer == 3)
+            ray = Physics2D.CircleCastAll(this.transform.position, col.radius,Vector2.zero, 0f ,LayerMask.GetMask("Ally"));
+        else if (this.gameObject.layer == 8)
+            ray = Physics2D.CircleCastAll(this.transform.position, col.radius, Vector2.zero, 0f, LayerMask.GetMask("Enemy"));
+        return ray;
     }
 }
